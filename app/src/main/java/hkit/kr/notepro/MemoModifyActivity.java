036f8cf798f0a2f78b5f467759b58b7a39959040
dnjstjr3741 <45345330+dnjstjr3741@users.noreply.github.com>
2018-11-27 16:42:24 +0900

    Initial commit

---
+++ app/src/main/java/hkit/kr/notepro/MemoModifyActivity.java
@@ -0,0 +1,44 @@
+package hkit.kr.notepro;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MemoModifyActivity extends AppCompatActivity {
+
+    Button modifyButton;
+    EditText memoEditText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_memo_modify);
+
+        Intent intent = getIntent();
+        String memo = intent.getStringExtra("memo");
+
+        memoEditText = findViewById(R.id.memoEditText);
+        modifyButton = findViewById(R.id.modifyButton);
+
+        memoEditText.setText(memo);
+
+        modifyButton.setOnClickListener(modifyListener);
+    }
+
+    View.OnClickListener modifyListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            String memo = memoEditText.getText().toString();
+
+            Intent data = new Intent();
+            data.putExtra("memo",memo);
+
+            setResult(Activity.RESULT_OK,data);
+            finish();
+        }
+    };
+}
